f0fd6e84ebe74134a28f81a1128288a13e99da9e
ahoshanhabib567 <68775154+ahoshanhabib567@users.noreply.github.com>
2023-06-22 00:45:31 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,18 @@
+<?php
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the submitted username and password
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
+  // Perform basic authentication
+  if ($username === 'admin' && $password === 'password123') {
+    // Authentication successful
+    echo 'Authentication successful! Welcome, ' . $username;
+  } else {
+    // Authentication failed
+    echo 'Authentication failed! Invalid username or password.';
+  }
+}
+?>
+  
